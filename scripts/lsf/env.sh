5db548ab577f97449757f8a2d8b0be95a5e1c3c6
Arush <23je0154@iitism.ac.in>
2026-07-05 14:31:08 -0400

    Switch W&B to online logging

--- scripts/lsf/env.sh
+++ scripts/lsf/env.sh
@@ -3,5 +3,4 @@
 export ALAB_SCRATCH="/u/arushh/alignmentlab_scratch"
 export HF_HOME="/u/arushh/.cache/huggingface"
 export WANDB_PROJECT="alignmentlab"
-# Remove this line after `wandb login` (then `wandb sync` any offline runs):
-export WANDB_MODE="offline"
+
